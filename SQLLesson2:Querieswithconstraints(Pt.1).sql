f3a443b91beb7bcaad53f91dd3e11bec62ef2241
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-06-28 12:24:29 +0200

    Update SQLLesson2:Querieswithconstraints(Pt.1).sql

--- SQLLesson2:Querieswithconstraints(Pt.1).sql
+++ SQLLesson2:Querieswithconstraints(Pt.1).sql
@@ -1,1 +1,4 @@
-
+SELECT * FROM movies where id = 6;
+SELECT * FROM movies where year >= 2000 AND year <= 2010;
+SELECT * FROM movies where NOT (year >= 2000 AND year <= 2010);
+SELECT * FROM movies limit 5;
