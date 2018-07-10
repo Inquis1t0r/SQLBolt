2f4e677d6f15b2d816cadecc2d988b00c1bfce56
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-07-10 08:20:42 +0200

    Create SQLLesson3:Querieswithconstraints(Pt.2).sql

---
+++ SQLLesson3:Querieswithconstraints(Pt.2).sql
@@ -0,0 +1,5 @@
+SELECT * FROM movies;
+SELECT * FROM movies WHERE Title LIKE "toy%";
+SELECT * FROM movies WHERE director = "John Lasseter";
+SELECT * FROM movies WHERE director != "John Lasseter";
+SELECT * FROM movies WHERE title Like "Wall-%";
