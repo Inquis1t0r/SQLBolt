063745451147962ae604262ec2c0f77b07395871
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-07-13 14:14:37 +0200

    Update SQLLesson4:FilteringandsortingQueryresults.sql

--- SQLLesson4:FilteringandsortingQueryresults.sql
+++ SQLLesson4:FilteringandsortingQueryresults.sql
@@ -1,1 +1,5 @@
-
+SELECT * FROM movies;
+SELECT DISTINCT Director FROM movies ORDER BY Director;
+SELECT * FROM movies ORDER BY year DESC limit 4;
+SELECT * FROM movies ORDER BY Title limit 5;
+SELECT * FROM movies ORDER BY Title limit 5 OFFSET 5;
