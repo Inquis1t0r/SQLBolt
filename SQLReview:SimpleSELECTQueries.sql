b917564617d0255d597aba5f1f226a2f4dd5fbaf
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-07-16 08:16:30 +0200

    Update SQLReview:SimpleSELECTQueries.sql

--- SQLReview:SimpleSELECTQueries.sql
+++ SQLReview:SimpleSELECTQueries.sql
@@ -1,6 +1,6 @@
 SELECT * FROM north_american_cities;
 SELECT * FROM north_american_cities WHERE Country = "Canada";
-SELECT * FROM north_american_cities WHERE Country = "United States" ORDER BY latitude DESC;
-SELECT city, longitude FROM north_american_cities WHERE Longitude < -87.629798 ORDER BY longitude ASC;
-SELECT city, population FROM north_american_citiesWHERE Country = "Mexico" ORDER BY population DESC LIMIT 2;
-SELECT city, population FROM north_american_cities WHERE Country = "United States" ORDER BY population DESC LIMIT 2 OFFSET 2;
+SELECT * FROM north_american_cities WHERE Country = "United States" ORDER BY Latitude DESC;
+SELECT * FROM north_american_cities WHERE Longitude < -87.629798 ORDER BY Longitude ASC;
+SELECT * FROM north_american_cities WHERE Country = "Mexico" ORDER BY Population DESC LIMIT 2;
+SELECT * FROM north_american_cities WHERE Country = "United States" ORDER BY Population DESC LIMIT 2 OFFSET 2;
